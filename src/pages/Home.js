e6b1f9ab12c2b47833ed07e0ed9a0ac7f0f5bf76
andre09999 <andre09999@live.com>
2022-11-11 18:47:20 -0300

    update css main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,13 @@ function Home({color}) {
        <span className="nome">Andre Luis</span><br/>
        Desenvolvedor Front_end</h1>
        <div className="containerbut">
-       <button className="but linkedin">
+       <a href='https://www.linkedin.com/in/andre-luis-6536b377/' target='_blank' className="but linkedin" rel="noreferrer">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" className="img-but"  alt='linkedin'/>
-           <a href='https://www.linkedin.com/in/andre-luis-6536b377/' target='_blank' className={color} rel="noreferrer">Linkedin</a>
-           </button>
-       <button className="but github">
+           <h2 className={color}>Linkedin</h2>
+           </a>
+       <a href='https://github.com/andre09999' target='_blank' className="but github" rel="noreferrer">
             <img  className="img-but" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github' />
-         <a  href='https://github.com/andre09999' target='_blank' className={color} rel="noreferrer"> Github</a></button>
+         <h2 className={color}> Github</h2></a>
        </div>
     </section>
     <section>
