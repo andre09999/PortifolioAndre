47a0058a6fbf24e866c98cf0f91f5d0942de3597
Andre <andre09999@live.com>
2024-01-04 17:12:39 -0300

    alterando portifolio

--- src/pages/Portifolio.js
+++ src/pages/Portifolio.js
@@ -1,105 +1,22 @@
-import foto from '../imgs/Projetos/App de receita.jpg'
-import foto1 from '../imgs/Projetos/Historia Do Android.jpg'
-import foto2 from '../imgs/Projetos/Online Store.jpg'
-import foto3 from '../imgs/Projetos/Starwars.jpg'
-import foto4 from '../imgs/Projetos/Trivia.jpg'
-import foto5 from '../imgs/Projetos/trynfu.jpg'
-import foto6 from '../imgs/Projetos/tune.jpg'
-import foto7 from '../imgs/Projetos/wallet.jpg'
-import foto8 from '../imgs/Projetos/Blackjack.jpg'
-import foto9 from '../imgs/Projetos/gastroBar.jpg'
-import foto10 from '../imgs/Projetos/PostagemSiteOliveerBarber.jpg'
-function Portifolio({color}) {
+import React from 'react';
+import projetos from '../dados/dados.json';
+
+function Portifolio({ color }) {
+  const imagens = require.context('../imgs/Projetos/', false, /\.(jpg)$/);
+  const fotos = imagens.keys().map(imagens).sort();
   return (
     <main id='portifolio' className='projetos-cont'>
-        <h1  id='titulo-Projetos' className={color}>Projetos</h1>
-        <section className='projetos'>
-        <a target='_blank' href='https://front-end-production-a1da.up.railway.app/login' rel="noreferrer"><img src={foto9} className="foto_portifolio" alt='avatar' />
-        <h2  id='titulo-home' className={color}>Gastro Bar</h2>
-        <p id='paragrafo' className={color}>Este projeto foi feito uma Api completa fullstack com validações de dados </p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-          Html, Css, Js, React, Node, Mysql, sequelize</span><br/>
-        </a>
-
-        <a target='_blank' href='https://oliveerbarber.netlify.app/' rel="noreferrer"><img src={foto10} className="foto_portifolio" alt='avatar' />
-
-<h2  id='titulo-home' className={color}>Projeto Oliveer Barber</h2>
-<p id='paragrafo' className={color}>Este trabalho Foi realizado toda parte de Front-end de maneira  responsiva </p>
-<span className='tecnologia'>Tecnologia utilizada:<br/>
-Html, Css, Js, React</span><br/>
-</a>
-
-        <a target='_blank' href='https://andre09999.github.io/App-De-Receitas/' rel="noreferrer"><img src={foto} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto App de receita</h2>
-        <p id='paragrafo' className={color}>Este projeto foi consumido 2 api de receitas uma de bebidas e outra de comida com parte para fazer login favoritar e finalizar receitas</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-
-        <a target='_blank' href='https://andre09999.github.io/projeto-android/' rel="noreferrer"><img src={foto1} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Historia do android</h2>
-        <p id='paragrafo' className={color}>Este projeto foi feito no curso do Gustavo guanabara no youtube</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css</span><br/>
-        </a>
-
-        <a target='_blank' href='https://andre09999.github.io/Store/' rel="noreferrer"><img src={foto2} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Online store</h2>
-        <p id='paragrafo' className={color}>Este projeto foi consumido api do mercado livre simulando um site para compras online</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-
-        <a target='_blank' href='https://andre09999.github.io/starwars-planets-search/' rel="noreferrer"><img src={foto3} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Starwars</h2>
-        <p id='paragrafo' className={color}>Este projeto foi consumido api do starwars falando sobre os planetas do filme e com filtros para localizar melhor o que procura</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-        <a target='_blank' href='https://andre09999.github.io/Trivia/' rel="noreferrer"><img src={foto4} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Trivia</h2>
-        <p id='paragrafo' className={color}>Este projeto foi consumido api do de jogo trivia</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-
-        <a target='_blank' href='https://andre09999.github.io/TrybeWarts/' rel="noreferrer"><img src={foto5} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Tryunfu</h2>
-        <p id='paragrafo' className={color}>Este projeto foi feito com base em jogo de tryunfu porem este e para cração de cartas</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-
-        <a target='_blank' href='https://andre09999.github.io/Tunes/' rel="noreferrer"><img src={foto6} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Tunes</h2>
-        <p id='paragrafo' className={color}>Este projeto foi feito consumindo api gratuita do itunes</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-
-        <a target='_blank' href='https://andre09999.github.io/Wallet/' rel="noreferrer"><img src={foto7} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Wallet</h2>
-        <p id='paragrafo' className={color}>Este projeto foi simulando uma carteira digital para monitorar os gastos fazendo soma conversão de valores</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-        
-        <a target='_blank' href='https://blackjackreact.netlify.app/' rel="noreferrer"><img src={foto8} className="foto_portifolio" alt='avatar' />
-
-        <h2  id='titulo-home' className={color}>Projeto Blackjack</h2>
-        <p id='paragrafo' className={color}>Este projeto foi Consumindo a API da klever e com um jogo Blackjack conhecido no Brasil com 21</p>
-        <span className='tecnologia'>Tecnologia utilizada:<br/>
-        Html, Css, Js, React</span><br/>
-        </a>
-        </section>
+      <h1 id='titulo-Projetos' className={color}>Projetos</h1>
+      <section className='projetos'>
+        {projetos.map((projeto, index) => (
+          <a key={index} target='_blank' href={projeto.link} rel="noreferrer">
+            <img src={fotos[index]} className="foto_portifolio" alt={projeto.nome} />
+            <h2 id='titulo-home' className={color}>{projeto.nome}</h2>
+            <p id='paragrafo' className={color}>{projeto.descricao}</p>
+            <span className='tecnologia'>Tecnologia utilizada:<br />{projeto.tecnologias}</span><br />
+          </a>
+        ))}
+      </section>
     </main>
   );
 }
--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -1,19 +1,26 @@
-import Avatar from '../imgs/fotoPessoal/315191556_5496258120490689_3663881283572641793_n.jpeg'
-function Sobre({color}) {
+import Avatar from '../imgs/fotoPessoal/315191556_5496258120490689_3663881283572641793_n.jpeg';
+import '../App.css';
+
+function Sobre({ color }) {
   return (
-    <main  id='sobre' className='container-geral'>
-         <section>
-    <img src={Avatar} className="pessoal" alt='avatar' />
-    </section>
-    <section className="texto-sec text">
-       <h1 id='historia' className={color}>SOBRE MIM</h1>
-       <h3 id='historia' className={color}>Goiânia, Goiás</h3>
-       <p id='historia' className={color}>Me chamo Andre,<br/>  Tenho 27 anos sou formado em gestão de recursos humanos e técnico em informatica tenho paixão pelaa area de tecnologia.
-<br/>
-Fiz curso de desenvolvimento web fullstack na trybe durante 1 ano para aperfeiçoar meus conhecimentos na area.
-<br/>
-Porem estudo nesta area de desenvolvimento web ha 2 anos e meio.</p>
-    </section>
+    <main id='sobre' className='container-geral'>
+      <section>
+        <img src={Avatar} className="pessoal" alt='avatar' />
+      </section>
+      <section className="texto-sec text">
+        
+          <h1 id='historia' className={color}>SOBRE MIM</h1>
+          <h3 id='historia' className={color}>Goiânia, Goiás</h3>
+          <p id='historia' className={color}>
+            Me chamo Andre,
+            <br /> Tenho 28 anos, sou formado em gestão de recursos humanos e técnico em informática e agora estou cursando Ciência da Computação, tenho paixão pela área de tecnologia.
+            <br />
+            Fiz curso de desenvolvimento web fullstack na Trybe durante 1 ano para aperfeiçoar meus conhecimentos na área.
+            <br />
+            Porém, estudo nesta área de desenvolvimento web há 2 anos e meio.
+          </p>
+      
+      </section>
     </main>
   );
 }
